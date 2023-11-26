f5fcd8503cfae0483bc6d83a48afe9696368d0fc
Tyreek Thomas <tyreek1thomas@gmail.com>
2023-11-26 11:47:56 -0600

    2

--- km Auto Rental/Client Sign-Up.aspx
+++ km Auto Rental/Client Sign-Up.aspx
@@ -58,7 +58,7 @@
     </div>
 
     <div class="mb-3">
-       <asp:Button id="signup" runat="server" Text="CREATE" />
+       <asp:Button id="signup" runat="server" Text="CREATE" OnClick="signup_Click" />
     </div>
 
 </div>
--- km Auto Rental/Client Sign-Up.aspx.cs
+++ km Auto Rental/Client Sign-Up.aspx.cs
@@ -1,16 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.CodeDom;
 
 namespace km_Auto_Rental
 {
     public partial class Client_Sign_Up : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void signup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO Customers (DriversNumber, FirstName, LastName, Email, Username, Password, Telephone) VALUES " +
+                    "(@driver_num, @fname, @lname, @email, @username, @passwrd, @telenum)", con);
+
+                cmd.Parameters.AddWithValue("@driver_num", dl.Text.Trim());
+                cmd.Parameters.AddWithValue("@fname", clfname.Text.Trim());
+                cmd.Parameters.AddWithValue("@lname", cllname.Text.Trim());
+                cmd.Parameters.AddWithValue("@email", emailc.Text.Trim());
+                cmd.Parameters.AddWithValue("@username", username.Text.Trim());
+                cmd.Parameters.AddWithValue("@passwrd", passrd.Text.Trim());
+                cmd.Parameters.AddWithValue("@telenum", PhnNu.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Sign Up Successful. Please go to Login');</script>");
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
 
         }
     }
